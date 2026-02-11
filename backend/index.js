6dd15ba07065e5b4aa6ecd633401f5281f8a0bfb
Prashant Aryan <prashantsuryavanahi51781@gmail.com>
2026-02-11 17:43:28 +0530

    Fix API endpoints for production

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:"https://job-portal-1-pjiu.onrender.com",
     credentials:true
 }
 
